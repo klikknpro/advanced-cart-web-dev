8d54431c47ead078b2e017226637d27e4dc3b2ec
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-07-28 14:29:24 +0200

    formatting the card

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import formatCurrency from "../utilities/formatCurrency";
 
 type StoreItemProps = {
   id: number;
@@ -10,10 +11,20 @@ type StoreItemProps = {
 
 const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
   return (
-    <Card>
-      <Card.Img variant="top" src={imgUrl} />
+    <Card className="store-item">
+      <Card.Img className="store-item-image" variant="top" src={imgUrl} height="200px" />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+      </Card.Body>
     </Card>
   );
 };
 
 export default StoreItem;
+
+/*
+video 25:03
+*/
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -5,7 +5,7 @@ import StoreItem from "../components/StoreItem";
 
 const Store = () => {
   return (
-    <div>
+    <div className="store">
       <h1>Store</h1>
       <Row xs={1} md={2} lg={3} className="g-3">
         {storeItems.map((item) => (
